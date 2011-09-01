759fcf15f098e9c5ed2cf0b973d36cac24b4357b
kaibin <ekaibwu@gmail.com>
2011-09-01 12:30:01 +0800

    add daily pushMessage limit to 5.

--- src/com/orange/groupbuy/pushserver/PushMessageRequest.java
+++ src/com/orange/groupbuy/pushserver/PushMessageRequest.java
@@ -135,7 +135,7 @@ public class PushMessageRequest extends BasicProcessorRequest {
     private void setPushMessageStatisticData(final PushMessage message) {
         message.put(DBConstants.F_PUSH_MESSAGE_START_DATE, startTime);
         message.put(DBConstants.F_PUSH_MESSAGE_FINISH_DATE, new Date());
-//        message.put(DBConstants.F_PUSH_MESSAGE_ERROR_CODE, result);
+        message.put(DBConstants.F_PUSH_MESSAGE_ERROR_CODE, result);
     }
 
 }
