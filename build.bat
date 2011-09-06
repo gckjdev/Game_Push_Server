7baae34fc8c107f4825c3a73f111419cdf5a1473
Benson Peng <penglingzhe@gmail.com>
2011-09-06 16:44:19 +0800

    update build files

--- build.bat
+++ build.bat
@@ -5,5 +5,5 @@ mvn clean install
 cd ../Common_Java_GroupBuy
 mvn clean install
 
-cd ../GroupBuy_API_Server
+cd ../GroupBuy_Push_Server
 mvn assembly:assembly
--- run_push_server.sh
+++ run_push_server.sh
@@ -1,1 +1,1 @@
-nohup java -jar GroupBuy_API_Server-1.0-SNAPSHOT.jar > ./api_server.log &
+nohup java -jar GroupBuy_Push_Server-1.0-SNAPSHOT.jar > ./push_server.log &
